b7b6eea64b929c60288b54c90d13a60fa299e168
RM-Terrell <terrellrmr@gmail.com>
2017-03-18 22:09:37 -0600

    First lesson on types

--- TS Template Test/ProjectREADME.txt
+++ TS Template Test/ProjectREADME.txt
@@ -8,6 +8,7 @@
 
 
 
+
 Needs more research:
 
 
--- TS Template Test/scripts.ts
+++ TS Template Test/scripts.ts
@@ -1,16 +1,32 @@
-﻿function Greeter(greeting: string) {
-    this.greeting = greeting;
-}
+﻿//function Greeter(greeting: string) {
+//    this.greeting = greeting;
+//}
 
-Greeter.prototype.greet = function () {
-    return "Hello, " + this.greeting;
-}
-let greeter = new Greeter("world");
+//Greeter.prototype.greet = function () {
+//    return "Hello, " + this.greeting;
+//}
+//let greeter = new Greeter("world");
 
-let button = document.createElement('button');
-button.textContent = "Say Hello";
-button.onclick = function () {
-    alert(greeter.greet());
-};
+//let button = document.createElement('button');
+//button.textContent = "Say Hello";
+//button.onclick = function () {
+//    alert(greeter.greet());
+//};
 
-document.body.appendChild(button);
+//document.body.appendChild(button);
+
+
+
+
+
+// String
+let myName = 'Max';
+
+//If i try to assign "myName" to a number an error will now be thrown.
+//Can also assign to numbers, EITHER float or integrer. TS doesnt care if the number has decimals, its all 'number'.
+
+// Boolean
+let hasHobbies = true;
+
+//If i try to assign = 1, will thrown and error.
+
