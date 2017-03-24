abc4c8034d9ed2c5f328f59630c3f513f0c96a1a
RM-Terrell <terrellrmr@gmail.com>
2017-03-23 19:02:59 -0600

    Post ex1

--- TS Template Test/Exercise1Section2.ts
+++ TS Template Test/Exercise1Section2.ts
@@ -1,13 +1,33 @@
 ﻿//Exercise goal to be as explicit as possible with given code.
 
-let bankAccount = {
+//let bankAccount = {
+//    money: 2000,
+//    deposit(value) {
+//        this.money += value;
+//    }
+//};
+
+//let myself = {
+//    name: "Max",
+//    bankAccount: bankAccount,
+//    hobbies: ["Sports", "Cooking"]
+//};
+
+//myself.bankAccount.deposit(3000);
+
+//console.log(myself);
+
+
+type BankAccount = { money: number, deposit: (value: number) => void };
+
+let bankAccount: {money: number, deposit: (value: number) => void} = {
     money: 2000,
-    deposit(value) {
+    deposit(value: number): void {
         this.money += value;
     }
 };
 
-let myself = {
+let myself: { name: string, bankAccount: BankAccount, hobbies:string[] } = {
     name: "Max",
     bankAccount: bankAccount,
     hobbies: ["Sports", "Cooking"]
