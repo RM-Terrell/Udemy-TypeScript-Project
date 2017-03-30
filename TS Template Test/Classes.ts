95e695fe15e78a584863c6752b2566cbc2da55cf
RM-Terrell <terrellrmr@gmail.com>
2017-03-30 17:48:18 -0600

    Inheritance and constructors

--- TS Template Test/Classes.ts
+++ TS Template Test/Classes.ts
@@ -23,3 +23,24 @@ console.log(person.name, person.username);
 
 person.printAge();
 person.setType("Cool guy"); //Wont work if private method
+
+// Inheritance
+
+class Max extends Person //Extends upon the person class and contains EVERYTHING it does
+{
+    name = "Max"; //Overwrites name with the given value
+}
+
+const max = new Max("Anna", "max"); //Anna will be overwritten by the above code. "max" will pass
+
+// Inheritance and constructors
+
+class Fred extends Person
+{
+    constructor(username: string) //Constructor now only expects one argument
+    {
+        super("Fred", username); //Must call super here in order to call constructor in the parent class
+
+    }
+}
+const fred = new Fred("fred");
