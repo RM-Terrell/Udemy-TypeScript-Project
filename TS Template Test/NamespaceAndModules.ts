c0fdd2c26e7b07d4d843fa33b4666a8b17f2a727
RM-Terrell <terrellrmr@gmail.com>
2017-04-03 13:34:03 -0600

    Namespaces

--- TS Template Test/NamespaceAndModules.ts
+++ TS Template Test/NamespaceAndModules.ts
@@ -1,1 +1,17 @@
-﻿
+﻿// Namespaces
+namespace MyMath
+{
+    const PI = 3.14;
+
+    export function calcCirc(diameter: number) //must use export to make accessable
+    {
+        return diameter * PI;
+    }
+
+    export function calcPerim(length: number, width: number)
+    {
+        return length * 2 + width * 2;
+    }
+}
+
+console.log(MyMath.calcCirc(2));
