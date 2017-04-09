301534c960f33341431ef6f82acdc8552f933389
RM-Terrell <terrellrmr@gmail.com>
2017-04-09 14:18:53 -0600

    method decorators

--- TS Template Test/Decorators.ts
+++ TS Template Test/Decorators.ts
@@ -1,6 +1,8 @@
 ﻿
 // Decorators
 
+// A decorator is a function. Any function. It is attached with an "@" 
+
 function logged(constructorFn: Function) {
 
     console.log(constructorFn);
@@ -14,6 +16,8 @@ class Personv2 { //lets add a decorator that logs info about the constructor in 
 }
 
 // Factory
+
+//Similiar too decorator but passes a value in () after it
 
 function logging(value: boolean) {
     return value ? logged : null //if true return logged function, if false return null
@@ -31,6 +35,7 @@ function printable(constructorFn: Function){
     }
 }
 
+// Can alsoo use multiple decorators if wanted. Just use another "@" on the next line
 @printable
 class Plant2 {
     name = "Green Plant";
@@ -38,3 +43,35 @@ class Plant2 {
 }
 const planbt = new Plant2();
 (<any>plant).print(); //has to be cast manually to type any for some reason. honestly not sure why
+
+// Method Decorator
+
+function editable(value: boolean)
+{ // we create a decorator to control if a value can be edited
+    return function (target: any, propName: string, descriptor: PropertyDescriptor)
+    // A descriptor is plain javascript. allows you to configure a property
+    {
+        descriptor.writable = value;
+    }
+}
+class Project2
+{
+    projectName: string;
+    constructor(name: string)
+    {
+        this.projectName = name;
+    }
+    @editable(false) // blocks editing
+    calcBudget()
+    {
+        console.log(1000);
+    }
+}
+
+const project = new Project2("Super Project");
+project.calcBudget();
+project.calcBudget = function ()
+{
+    console.log(2000);
+}
+project.calcBudget(); // without the descritpor will return 1000, then 2000, because we are overwriting the function
