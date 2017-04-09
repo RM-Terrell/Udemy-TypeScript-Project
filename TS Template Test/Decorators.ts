44e97d28812d5953c18ddd402c2e264893942c99
RM-Terrell <terrellrmr@gmail.com>
2017-04-09 14:48:27 -0600

    Final decorators notes

--- TS Template Test/Decorators.ts
+++ TS Template Test/Decorators.ts
@@ -44,7 +44,8 @@ class Plant2 {
 const planbt = new Plant2();
 (<any>plant).print(); //has to be cast manually to type any for some reason. honestly not sure why
 
-// Method Decorator
+// Method Decorator and Property decorators
+// Due to some techincal difficulties, property and methods cant accessed in the same way, so the two are fairley different
 
 function editable(value: boolean)
 { // we create a decorator to control if a value can be edited
@@ -54,8 +55,20 @@ function editable(value: boolean)
         descriptor.writable = value;
     }
 }
+
+function overwritable(value: boolean) //Property descciptor
+{
+    return function (target: any, propName: string): any //No decriptor. cant be accessed. but can be set
+    {
+        const newDescriptor: PropertyDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    }
+}
 class Project2
 {
+    @overwritable(false) //An error will result here if line 61 :any isnt set. all writing access is prevented by false, so now value set at all. 
     projectName: string;
     constructor(name: string)
     {
@@ -75,3 +88,36 @@ project.calcBudget = function ()
     console.log(2000);
 }
 project.calcBudget(); // without the descritpor will return 1000, then 2000, because we are overwriting the function
+
+// Parameter Decorators
+
+function printInfo(target: any, methodName: string, paramIndex: number)
+{
+    console.log("Target:", target);
+    console.log("methodName:", methodName);
+    console.log("paramIndex:", paramIndex);
+
+}
+
+class Course
+{
+    name: string;
+    constructor(name: string)
+    {
+        this.name = name;
+    }
+
+    printStudentNumbers(mode: string, @printInfo printAll: boolean)
+    {
+        if (printAll) {
+            console.log(1000);
+        }
+        else {
+            console.log(2000);
+        }
+    }
+}
+
+const course = new Course("Super Course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);  // return 1000 and 2000 without decorator. returns all the other data with it
